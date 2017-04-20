76ef67cdd54de8670e43ad57e2a1cef18d78746b
zcunique <zengchen94@gmail.com>
2017-04-20 21:12:24 +0800

    dp

---
+++ 10. Regular Expression Matching.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) 
+            return false;
+
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*') {//can match zero
+                dp[0][i+1] = dp[0][i-1];
+            }
+        }
+        for (int i = 0 ; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {//match single one
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {//can only match zero
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    } else {
+                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);//match single one || multiple one || zero
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+}

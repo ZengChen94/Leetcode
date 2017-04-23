93cae6f0d7a38c0b7281de79e909e0179e3d106c
zcunique <zengchen94@gmail.com>
2017-04-23 22:41:38 +0800

    permutation dfs

---
+++ 47. Permutations II.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        LinkedList<List<Integer>> result = new LinkedList<List<Integer>>();
+        ArrayList<Integer> tmpResult = new ArrayList<Integer>();
+        result.add(tmpResult);
+        
+        //the order of permulation doesn't count too much
+        for (int j = 0; j < nums.length; j++) {
+            int n = nums[j];
+            int size = result.size();
+            for (; size > 0; size--) {
+                List<Integer> r = result.get(0);
+                result.remove(0);
+                for (int i = 0; i <= r.size(); i++) {
+                    List<Integer> tmp = new ArrayList<Integer>(r);
+                    tmp.add(i, n);
+                    if (result.contains(tmp) != true)
+                        result.add(tmp);
+                }
+            }
+        }
+        return result;
+    }
+}

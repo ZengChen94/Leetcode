d85adecd7eb95a38eaceedbea8233cbdb702e8d1
zcunique <zengchen94@gmail.com>
2017-04-24 10:05:55 +0800

    make order and then insert can largely increase efficiency

--- 47. Permutations II.java
+++ 47. Permutations II.java
@@ -4,18 +4,31 @@ public class Solution {
         ArrayList<Integer> tmpResult = new ArrayList<Integer>();
         result.add(tmpResult);
         
+        Arrays.sort(nums);
         //the order of permulation doesn't count too much
+        //to avoid duplicate, we need to make the right order as for the same element
         for (int j = 0; j < nums.length; j++) {
             int n = nums[j];
             int size = result.size();
             for (; size > 0; size--) {
                 List<Integer> r = result.get(0);
                 result.remove(0);
-                for (int i = 0; i <= r.size(); i++) {
+                
+                int start = 0;
+                if (j > 0 && nums[j] == nums[j-1]){
+                    for (int k = r.size()-1; k >= 0; k--){
+                        if (r.get(k) == n){
+                            start = k+1;
+                            break;
+                        }
+                    }
+                }
+                
+                for (int i = start; i <= r.size(); i++) {
                     List<Integer> tmp = new ArrayList<Integer>(r);
                     tmp.add(i, n);
-                    if (result.contains(tmp) != true)
-                        result.add(tmp);
+                    //if (result.contains(tmp) != true)
+                    result.add(tmp);
                 }
             }
         }

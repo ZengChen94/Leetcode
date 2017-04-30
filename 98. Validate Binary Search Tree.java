a9b91e9c7c56fba0756f168b68e2fab4a30b7207
zcunique <zengchen94@gmail.com>
2017-04-30 12:46:25 +0800

    using Long to avoid overflow comparing and using recursive from top to bottom

---
+++ 98. Validate Binary Search Tree.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    boolean isValidBST(TreeNode root, long lower, long upper) {
+        if (root == null) return true;
+        return root.val > lower && root.val < upper && isValidBST(root.left, lower, root.val) && isValidBST(root.right, root.val, upper);
+    }
+}

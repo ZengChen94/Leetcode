a09379e0abdfec7fc84c7e782b3b917c2537385e
zcunique <zengchen94@gmail.com>
2017-05-07 11:20:36 +0800

    insertion sort & merge sort for linked list

--- 147. Insertion Sort List.java
+++ 147. Insertion Sort List.java
@@ -8,25 +8,37 @@
  */
 public class Solution {
     public ListNode insertionSortList(ListNode head) {
-        if(head == null) return head;
+        if(head == null || head.next == null) return head;
 		
-		ListNode helper = new ListNode(0);
+		ListNode run = head.next;
+		ListNode tail = head;
 		
-		ListNode cur = head;
-		ListNode pre = helper;
-		ListNode next = null;
-
-		while(cur != null){
-			next = cur.next;
-			while(pre.next != null && pre.next.val < cur.val){
-				pre = pre.next;
-			}
-			cur.next = pre.next;
-			pre.next = cur;
-			pre = helper;
-			cur = next;
+		while (run != null) {
+		    ListNode cur = run;
+		    run = run.next;
+		    if (cur.val <= head.val) {
+		        tail.next = run;
+		        cur.next = head;
+		        head = cur;
+		    }
+		    else if (cur.val >= tail.val) {
+		        tail = cur;
+		        continue;
+		    }
+		    else {
+		        ListNode run2 = head;
+		        while (run2 != tail) {
+		            if (run2.val < cur.val && cur.val < run2.next.val || run2.val == cur.val) {
+		                tail.next = run;
+		                cur.next = run2.next;
+		                run2.next = cur;
+		                break;
+		            }
+		            run2 = run2.next;
+		        }
+		    }
 		}
 		
-		return helper.next;
+		return head;
     }
 }

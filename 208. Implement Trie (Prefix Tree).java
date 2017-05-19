c6f2b34ae03d600d54a689274d9e4118032982aa
zcunique <zengchen94@gmail.com>
2017-05-19 13:07:49 +0800

    trie tree

--- 208. Implement Trie (Prefix Tree).java
+++ 208. Implement Trie (Prefix Tree).java
@@ -1,9 +1,7 @@
-//在计算机科学中，trie，又称前缀树或字典树，是一种有序树，用于保存关联数组，其中的键通常是字符串。与二叉查找树不同，键不是直接保存在节点中，而是由节点在树中的位置决定。一个节点的所有子孙都有相同的前缀，也就是这个节点对应的字符串，而根节点对应空字符串。一般情况下，不是所有的节点都有对应的值，只有叶子节点和部分内部节点所对应的键才有相关的值。
-
 class TrieNode {
     public char val;
     public boolean isWord; //important
-    public TrieNode[] children = new TrieNode[26];//important //using array to simulate map
+    public TrieNode[] children = new TrieNode[26];
     public TrieNode() {}
     TrieNode(char c){
         TrieNode node = new TrieNode();
@@ -12,46 +10,50 @@ class TrieNode {
 }
 
 public class Trie {
-
+    
     private TrieNode root;
     
     /** Initialize your data structure here. */
     public Trie() {
-        root = new TrieNode();
-        root.val = ' ';
+        root = new TrieNode(' ');
     }
     
     /** Inserts a word into the trie. */
     public void insert(String word) {
-        TrieNode ws = root;
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
-            if(ws.children[c - 'a'] == null){
-                ws.children[c - 'a'] = new TrieNode(c);
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (cur.children[word.charAt(i)-'a'] == null) {
+                TrieNode tmp = new TrieNode(word.charAt(i));
+                cur.children[word.charAt(i)-'a'] = tmp;
             }
-            ws = ws.children[c - 'a'];
+            cur = cur.children[word.charAt(i)-'a'];
         }
-        ws.isWord = true;
+        cur.isWord = true;
     }
     
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        TrieNode ws = root; 
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
-            if(ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (cur.children[word.charAt(i)-'a'] == null)
+                return false;
+            else
+                cur = cur.children[word.charAt(i)-'a'];
         }
-        return ws.isWord;
+        if (cur.isWord == true)
+            return true;
+        else
+            return false;
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
     public boolean startsWith(String prefix) {
-        TrieNode ws = root; 
-        for(int i = 0; i < prefix.length(); i++){
-            char c = prefix.charAt(i);
-            if(ws.children[c - 'a'] == null) return false;
-            ws = ws.children[c - 'a'];
+        TrieNode cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (cur.children[prefix.charAt(i)-'a'] == null)
+                return false;
+            else
+                cur = cur.children[prefix.charAt(i)-'a'];
         }
         return true;
     }

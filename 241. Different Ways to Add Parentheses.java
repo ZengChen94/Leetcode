4d7796c9c8ff38f94814daad1adafff38cc7bf43
zcunique <zengchen94@gmail.com>
2017-05-28 21:20:55 +0800

    commit

---
+++ 241. Different Ways to Add Parentheses.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> result = new LinkedList<Integer>();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '-' || input.charAt(i) == '*' || input.charAt(i) == '+' ) {
+                List<Integer> part1 = diffWaysToCompute(input.substring(0, i));
+                List<Integer> part2 = diffWaysToCompute(input.substring(i+1));
+                for (Integer p1 : part1) {
+                    for (Integer p2 : part2) {
+                        int c = 0;
+                        if (input.charAt(i) == '+')
+                            c = p1 + p2;
+                        else if (input.charAt(i) == '-')
+                            c = p1 - p2;
+                        else if (input.charAt(i) == '*')
+                            c = p1 * p2;
+                        result.add(c);
+                    }
+                }
+            }
+        }
+        if (result.size() == 0) {
+            result.add(Integer.valueOf(input));
+        }
+        return result;
+    }
+}

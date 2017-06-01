126cd9e49b3ab1dc4c67e355910c77a3df5e8616
zcunique <zengchen94@gmail.com>
2017-06-01 21:40:13 +0800

    String.equals(String)

--- 290. Word Pattern.java
+++ 290. Word Pattern.java
@@ -1,18 +1,5 @@
 public class Solution {
     public boolean wordPattern(String pattern, String str) {
-        String[] words = str.split(" ");//important
-        if (words.length != pattern.length())
-            return false;
-        Map index = new HashMap();
-        for (Integer i=0; i<words.length; ++i)
-            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
-                return false;
-        return true;
-    }
-}
-
-public class Solution {
-    public boolean wordPattern(String pattern, String str) {
         String[] words = str.split(" ");
         
         if (words.length != pattern.length())
@@ -21,10 +8,13 @@ public class Solution {
         
         for (int i = 0; i < words.length; i++) {
             if (map.containsKey(pattern.charAt(i))) {
-                if (map.get(pattern.charAt(i)) != words[i])
+                if (!map.get(pattern.charAt(i)).equals(words[i]))
                     return false;
             }
             else {
+                for (String value : map.values())
+                    if (value.equals(words[i]))
+                        return false;
                 map.put(pattern.charAt(i), words[i]);
             }
         }

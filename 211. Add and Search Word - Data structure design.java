ee045168327311c099553f5ac50011ffdfce1740
zcunique <zengchen94@gmail.com>
2017-06-02 19:25:33 +0800

    Trie

--- 211. Add and Search Word - Data structure design.java
+++ 211. Add and Search Word - Data structure design.java
@@ -1,63 +1,57 @@
 public class WordDictionary {
-    //trie based solution
     
-    WordNode root = new WordNode();
-	public void addWord(String word) {
-		char chars[] = word.toCharArray();
-        addWord(chars, 0, root);
+    private class WordNode{
+    	boolean isLeaf;
+    	WordNode[] children = new WordNode[26];
     }
-	
-	private void addWord(char[] chars, int index, WordNode parent) {
-		char c = chars[index];
-		int idx = c-'a';
-        WordNode node = parent.children[idx];
-        if (node == null){
-        	node = new WordNode();
-        	parent.children[idx]=node;
-        }
-        if (chars.length == index+1){
-        	node.isLeaf=true;
-        	return;
-        }
-        addWord(chars, ++index, node);
+    WordNode root;
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new WordNode();
     }
-
-
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        char[] chars = word.toCharArray();
+        WordNode cur = root;
+        for (int i = 0; i < chars.length; i++) {
+            int index = chars[i]-'a';
+            if (cur.children[index] == null) {
+                WordNode newNode = new WordNode();
+                cur.children[index] = newNode;
+            }
+            cur = cur.children[index];  
+        }
+        cur.isLeaf = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
     public boolean search(String word) {
-    	return search(word.toCharArray(), 0, root);				     
+        char[] chars = word.toCharArray();
+        return search(chars, 0, root);
     }
     
-    private boolean search(char[] chars, int index, WordNode parent){
+    public boolean search(char[] chars, int index, WordNode parent){
     	if (index == chars.length){
-    		if (parent.isLeaf){
+    		if (parent.isLeaf)
     			return true;
-    		}
-    		return false;
+    		else
+    		    return false;
     	}
-    	WordNode[] childNodes = parent.children;
-    	char c = chars[index];
-    	if (c == '.'){
-	    	for (int i=0;i<childNodes.length;i++){
-	    		WordNode n = childNodes[i];
-	    		if (n !=null){
-	    			boolean b = search(chars, index+1, n);
-	    			if (b){
+    	if (chars[index] == '.'){
+	    	for (int i = 0; i < 26; i++){
+	    		if (parent.children[i] != null){
+	    			if (search(chars, index+1, parent.children[i]))
 	    				return true;
-	    			}
 	    		}
 	    	}
 	    	return false;
     	}
-    	WordNode node = childNodes[c-'a'];
-    	if (node == null){
+    	WordNode node = parent.children[chars[index]-'a'];
+    	if (node == null)
     		return false;
-    	}
-    	return search(chars, ++index, node);
-    }
-
-    private class WordNode{
-    	boolean isLeaf;
-    	WordNode[] children = new WordNode[26];
+    	return search(chars, index+1, node);
     }
 }
 

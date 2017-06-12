6138c2374fb4f04ad36bd67f096d7a0eda747c92
zcunique <zengchen94@gmail.com>
2017-06-12 20:03:24 +0800

    TreeMap lowerKey/higherKey

---
+++ 352. Data Stream as Disjoint Intervals.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class SummaryRanges {
+
+    TreeMap<Integer, Interval> tree;
+
+    /** Initialize your data structure here. */
+    public SummaryRanges() {
+        tree = new TreeMap<>();
+    }
+    
+    public void addNum(int val) {
+        if(tree.containsKey(val)) 
+            return;
+        Integer low = tree.lowerKey(val);
+        Integer high = tree.higherKey(val);
+        if(low != null && high != null && tree.get(low).end + 1 == val && high == val + 1) {
+            tree.get(low).end = tree.get(high).end;
+            tree.remove(high);
+        } 
+        else if(low != null && tree.get(low).end + 1 >= val) {
+            tree.get(low).end = Math.max(tree.get(low).end, val);
+        } 
+        else if(high != null && high == val + 1) {
+            tree.put(val, new Interval(val, tree.get(high).end));
+            tree.remove(high);
+        } 
+        else {
+            tree.put(val, new Interval(val, val));
+        }
+    }
+    
+    public List<Interval> getIntervals() {
+        return new ArrayList<>(tree.values());
+    }
+}
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.addNum(val);
+ * List<Interval> param_2 = obj.getIntervals();
+ */

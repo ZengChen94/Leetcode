a3808e71aa58c5e23f9df74394cc1eba9f1c804d
zcunique <zengchen94@gmail.com>
2017-06-23 14:27:24 +0800

    commit

--- 386. Lexicographical Numbers.java
+++ 386. Lexicographical Numbers.java
@@ -1,22 +1,19 @@
 public class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> list = new ArrayList<>(n);
-        int curr = 1;
-        for (int i = 1; i <= n; i++) {
-            list.add(curr);
-            if (curr * 10 <= n) {
-                curr *= 10;
-            } 
-            else if (curr % 10 != 9 && curr + 1 <= n) {
-                curr++;
-            } 
-            else {
-                while ((curr / 10) % 10 == 9) {
-                    curr /= 10;
-                }
-                curr = curr / 10 + 1;
+        List<Integer> res = new ArrayList<>(n);
+        int cur = 1;
+        for (int i = 0; i < n; ++i) {
+            res.add(cur);
+            if (cur * 10 <= n) {
+                cur *= 10;
+            } else {
+                if (cur >= n) 
+                    cur /= 10;
+                cur += 1;
+                while (cur % 10 == 0) 
+                    cur /= 10;
             }
         }
-        return list;
+        return res;
     }
 }

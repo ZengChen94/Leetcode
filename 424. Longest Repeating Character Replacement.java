bfc8091d1ecc243fcec8659a1e270c6965f10696
zcunique <zengchen94@gmail.com>
2017-07-11 19:53:50 +0800

    maxCount

---
+++ 424. Longest Repeating Character Replacement.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int characterReplacement(String s, int k) {
+        int len = s.length();
+        int[] count = new int[26];
+        int start = 0, maxCount = 0, maxLength = 0;
+        
+        for (int end = 0; end < len; end++) {
+            count[s.charAt(end) - 'A'] += 1;
+            maxCount = Math.max(maxCount, count[s.charAt(end) - 'A']);
+            while (end - start + 1 - maxCount > k) {
+                count[s.charAt(start) - 'A']--;
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        
+        return maxLength;
+    }
+}

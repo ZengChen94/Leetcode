06c8091ccd7a8f0f9ce70fe47879b182e3464958
zcunique <zengchen94@gmail.com>
2017-07-12 10:38:27 +0800

    preSum

---
+++ 437. Path Sum III.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();// key : the prefix sum, value : how many ways get to this prefix sum
+        preSum.put(0, 1);
+        helper(root, 0, sum, preSum);
+        return count;
+    }
+    int count = 0;
+    //currSum: from root to every node
+    public void helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return;
+        }
+        
+        currSum += root.val;
+
+        if (preSum.containsKey(currSum - target)) {
+            count += preSum.get(currSum - target);
+        }
+        
+        if (!preSum.containsKey(currSum)) {
+            preSum.put(currSum, 1);
+        } 
+        else {
+            preSum.put(currSum, preSum.get(currSum)+1);
+        }
+        
+        helper(root.left, currSum, target, preSum);
+        helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
+    }
+}

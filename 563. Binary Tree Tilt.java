04b1cb5c7ff5379a5ea0570270eda1e6ad5a6dab
zcunique <zengchen94@gmail.com>
2017-07-19 14:47:33 +0800

    commit

--- 563. Binary Tree Tilt.java
+++ 563. Binary Tree Tilt.java
@@ -8,23 +8,21 @@
  * }
  */
 public class Solution {
-    public int findTilt(TreeNode root) {
-        int result = 0;
+    int result = 0;
     
-        public int findTilt(TreeNode root) {
-            postOrder(root);
-            return result;
-        }
-
-        private int postOrder(TreeNode root) {
-            if (root == null) return 0;
-
-            int left = postOrder(root.left);
-            int right = postOrder(root.right);
-
-            result += Math.abs(left - right);
-
-            return left + right + root.val;
-        }
+    public int findTilt(TreeNode root) {
+        postOrder(root);
+        return result;
+    }
+    
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+        
+        result += Math.abs(left - right);
+        
+        return left + right + root.val;
     }
 }

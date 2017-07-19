2eb80400e6837e92f5c77438240c08eca86574f2
zcunique <zengchen94@gmail.com>
2017-07-19 14:46:49 +0800

    commit

---
+++ 553. Optimal Division.java
@@ -0,0 +1,17 @@
+//the answer is always X1/(X2/X3/../Xn) 
+
+public class Solution {
+    public String optimalDivision(int[] nums) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (i == 1 && nums.length > 2) {
+                builder.append("/(").append(nums[i]);
+            } else {
+                builder.append("/").append(nums[i]);
+            }
+        }
+        
+        return nums.length > 2 ? builder.append(")").toString() : builder.toString();
+    }
+}

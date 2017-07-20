32d4a00ca0d3336db04b3782d849102261d78051
zcunique <zengchen94@gmail.com>
2017-07-20 12:16:26 +0800

    Graham Algorithm

---
+++ 587. Erect the Fence.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a point.
+ * class Point {
+ *     int x;
+ *     int y;
+ *     Point() { x = 0; y = 0; }
+ *     Point(int a, int b) { x = a; y = b; }
+ * }
+ */
+
+
+//http://blog.csdn.net/bone_ace/article/details/46239187
+//Graham Algorithm
+
+public class Solution {
+    public List<Point> outerTrees(Point[] points) {
+        
+    }
+}

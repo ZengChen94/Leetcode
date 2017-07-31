96b499a183c0cf8c6eefe6ef2b99c522417c7d95
zcunique <zengchen94@gmail.com>
2017-07-31 18:32:01 +0800

    dp

--- 10. Regular Expression Matching.java
+++ 10. Regular Expression Matching.java
@@ -12,14 +12,14 @@ public class Solution {
         }
         for (int i = 0 ; i < s.length(); i++) {
             for (int j = 0; j < p.length(); j++) {
-                if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {//match single one
+                if (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i)) {//'.' or 'a-z' --> match single one
                     dp[i+1][j+1] = dp[i][j];
                 }
-                if (p.charAt(j) == '*') {
-                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {//can only match empty
+                if (p.charAt(j) == '*') {//'*'
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {//can ONLY match empty
                         dp[i+1][j+1] = dp[i+1][j-1];
                     } else {
-                        dp[i+1][j+1] = (dp[i][j-1] || dp[i][j+1] || dp[i+1][j-1]);//match single one || multiple one || empty
+                        dp[i+1][j+1] = (dp[i][j-1] || dp[i][j+1] || dp[i+1][j-1]);//can match single one OR multiple one OR empty
                     }
                 }
             }

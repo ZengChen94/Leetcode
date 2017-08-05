ad3c4318e1885949ed0cc010245bbbddfaab883a
zcunique <zengchen94@gmail.com>
2017-08-05 09:20:28 +0800

    can compress the map into one dimension

--- 52. N-Queens II.java
+++ 52. N-Queens II.java
@@ -50,3 +50,38 @@ public class Solution {
         return true;
     } 
 }
+
+public class Solution {
+	public int totalNQueens(int n) {  
+        int[] res = {0};
+        if(n<=0)
+            return res[0];
+            
+        int [] columnVal = new int[n];
+        
+        DFS_helper(n,res,0,columnVal);
+        return res[0];
+    }
+    
+    public void DFS_helper(int nQueens, int[] res, int row, int[] columnVal){
+        if(row == nQueens){
+            res[0] += 1;
+        }else{
+            for(int i = 0; i < nQueens; i++){
+                columnVal[row] = i;//(row,columnVal[row)==>(row,i)
+                
+                if(isValid(row,columnVal))
+                    DFS_helper(nQueens, res, row+1, columnVal);
+            }
+        }
+    }
+    
+    public boolean isValid(int row, int [] columnVal){
+        for(int i = 0; i < row; i++){
+            if(columnVal[row] == columnVal[i]
+               ||Math.abs(columnVal[row]-columnVal[i]) == row-i)
+               return false;
+        }
+        return true;
+	}
+}

ef85308a448c89c03fa856f6396a6025f28c1c1d
zcunique <zengchen94@gmail.com>
2017-08-23 08:46:09 -0500

    method2 is better, which is similar to 109
    the global var can record the trace

--- 114. Flatten Binary Tree to Linked List.java
+++ 114. Flatten Binary Tree to Linked List.java
@@ -28,3 +28,18 @@ public class Solution {
         root = dummy.right;
     }
 }
+
+
+public class Solution {
+    private TreeNode prev = null;
+
+	public void flatten(TreeNode root) {
+		if (root == null)
+			return;
+		flatten(root.right);
+		flatten(root.left);
+		root.right = prev;
+		root.left = null;
+		prev = root;
+	}
+}

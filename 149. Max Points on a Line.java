f8b35fbdf22813c427beb06317a883e1a5ae4739
zcunique <zengchen94@gmail.com>
2017-08-25 08:20:16 -0500

    key, value = slope, number of points
    don't forget about the duplicate

--- 149. Max Points on a Line.java
+++ 149. Max Points on a Line.java
@@ -8,37 +8,37 @@
  * }
  */
  /*
- 点到点斜率为key 该斜率点数为value 不断更新
+ key, value = slope, number of points
  */
  
 public class Solution{
     public int maxPoints(Point[] points) {
     	if (points.length < 3) return points.length;  
           
-        int max = 0;//用于返回的结果，即共线点的最大个数  
-        Map<Double, Integer> map = new HashMap<Double, Integer>();//保存同一个斜率的点的个数  
+        int max = 0; 
+        Map<Double, Integer> map = new HashMap<Double, Integer>();
           
-        for (int i = 0; i < points.length; i++) {//以每一个点为固定点  
+        for (int i = 0; i < points.length; i++) {//set every points 
             map.clear();  
-            int duplicate = 1;//记录跟固定点重合的个数  
+            int duplicate = 1;//record duplicate!!! 
               
-            for(int j = 0 ; j < points.length; j++){//遍历其他点，求其与固定点之间的斜率  
-                if (i == j) continue;//如果是自己，跳过  
-                double slope = 0.0;//斜率  
+            for(int j = 0 ; j < points.length; j++){//travel
+                if (i == j) continue;
+                double slope = 0.0;//slope  
                   
-                if (points[i].x == points[j].x && points[i].y == points[j].y) {//如果跟固定点重合  
+                if (points[i].x == points[j].x && points[i].y == points[j].y) {//duplicate
                     duplicate++;  
                     continue;  
-                } else if (points[i].x == points[j].x) {//如果跟固定点在同一条竖线上，斜率设为最大值  
+                } else if (points[i].x == points[j].x) {//slope = max_limit --> Integer.MAX_VALUE  
                     slope = Integer.MAX_VALUE;  
-                } else {//计算该点与固定点的斜率  
+                } else {//slope
                     slope = 1.0 * (points[i].y - points[j].y) / (points[i].x - points[j].x);  
                 }  
                 map.put(slope, map.containsKey(slope) ? map.get(slope) + 1 : 1);  
             }  
               
-            //更新最优解  
-            if (map.keySet().size() == 0) {//如果map为空  
+            //update  
+            if (map.keySet().size() == 0) {//think of duplicate!!! 
                 max = duplicate > max ? duplicate : max;  
             } else {  
                 for (double key : map.keySet()) {  

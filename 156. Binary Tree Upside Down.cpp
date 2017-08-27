d1d3239acaf62c34046e1bbc2fcdaeb6741639b2
zcunique <zengchen94@gmail.com>
2017-08-27 14:15:55 -0500

    iteration

---
+++ 156. Binary Tree Upside Down.cpp
@@ -0,0 +1,22 @@
+//http://blog.csdn.net/whuwangyi/article/details/43186045
+
+/** 
+ * Definition for a binary tree node. 
+ * struct TreeNode { 
+ *     int val; 
+ *     TreeNode *left; 
+ *     TreeNode *right; 
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {} 
+ * }; 
+ */  
+class Solution {  
+public:  
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {  
+        if(!root || !root->left) return root;  
+        TreeNode *newRoot = upsideDownBinaryTree(root->left);  
+        root->left->left = root->right;  
+        root->left->right = root;  
+        root->left = NULL, root->right = NULL;  
+        return newRoot;  
+    }  
+};  

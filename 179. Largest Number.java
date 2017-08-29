6c66e6a38ff077c5260a15b8566cfeb646a13e3f
zcunique <zengchen94@gmail.com>
2017-08-29 11:04:30 -0500

    String.compareTo(String)

--- 179. Largest Number.java
+++ 179. Largest Number.java
@@ -6,7 +6,7 @@ public class Solution {
 		String[] s_num = new String[num.length];
 		for(int i = 0; i < num.length; i++)
 		    s_num[i] = String.valueOf(num[i]);
-		
+		/*
 		Comparator<String> comp = new Comparator<String>(){
             @Override
             public int compare(String str1, String str2){
@@ -15,8 +15,8 @@ public class Solution {
                 return s2.compareTo(s1);
             }
         };
-        
-        Arrays.sort(s_num, comp);
+        */
+        Arrays.sort(s_num, ((num1, num2) -> ((num2+num1).compareTo(num1+num2))));
         if(s_num[0].charAt(0) == '0')
             return "0";
          

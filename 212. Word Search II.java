361d2fa1aa0617f4014975c930edcc08db28550b
zcunique <zengchen94@gmail.com>
2017-08-29 11:05:02 -0500

    TrieTree

--- 212. Word Search II.java
+++ 212. Word Search II.java
@@ -3,7 +3,7 @@
 public class Solution {
     public List<String> findWords(char[][] board, String[] words) {
         List<String> res = new ArrayList<>();
-        TrieNode root = buildTrie(words);//important
+        TrieNode root = buildTrie(words);//important //initially construct every words to TrieNode
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 dfs (board, i, j, root, res);

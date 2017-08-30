972b6b61501c4b78d09f3415158750ac2f04c9f1
zcunique <zengchen94@gmail.com>
2017-08-29 22:15:07 -0500

    priority queue: store heighest before (travel from left to right)

--- 218. The Skyline Problem.java
+++ 218. The Skyline Problem.java
@@ -1,44 +1,31 @@
-public class Solution {
-    public List<int[]> getSkyline(int[][] buildings) {
+class Solution {
+    public List<int[]> getSkyline(int[][] buildings) {//L, R, H
         List<int[]> result = new ArrayList<>();
-        if (buildings.length == 0) return result;
-        int max = 0;
-        for (int i = 0; i < buildings.length; i++) {
-            if (buildings[i][1] > max)
-                max = buildings[i][1];
+        List<int[]> height = new ArrayList<>();
+        for(int[] b : buildings) {
+            height.add(new int[]{b[0], -b[2]});//push(L, -H)
+            height.add(new int[]{b[1], b[2]});//push(R, H)
         }
-        if (buildings.length == 2 && buildings[0][0] == 1 && buildings[0][1] == 2 && buildings[0][2] == 1 && buildings[1][0] == 2147483646 && buildings[1][1] == 2147483647 && buildings[1][2] == 2147483647){
-            result.add(new int[]{1,1});
-            result.add(new int[]{2,0});
-            result.add(new int[]{2147483646,2147483647});
-            result.add(new int[]{2147483647,0});
-            return result;
-        }
-        if (max == 2147483647) {
-            result.add(new int[]{0, 2147483647});
-            result.add(new int[]{2147483647, 0});
-            return result;
-        }
-        
-        int[] heightList = new int[max+1];
-        for (int i = 0; i < buildings.length; i++) {
-            int left = buildings[i][0];
-            int right = buildings[i][1];
-            int height = buildings[i][2];
-            for (int j = left; j <= right; j++) 
-                heightList[j] = height > heightList[j]? height : heightList[j];
-        }
-        if (heightList[0] != 0)
-            result.add(new int[]{0, heightList[0]});
-        for (int i = 1; i < heightList.length-1; i++) {
-            if (heightList[i] > heightList[i-1]) {
-                result.add(new int[]{i, heightList[i]});
+        Collections.sort(height, (a, b) -> {
+                if(a[0] != b[0]) 
+                    return a[0] - b[0];
+                return a[1] - b[1];
+        });//sort accoding to left to right, then from low to high
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));//from large to small
+        pq.offer(0);
+        int prev = 0;//pre highest
+        for(int[] h : height) {
+            if(h[1] < 0) {//left --> up
+                pq.offer(-h[1]);
+            } else {
+                pq.remove(h[1]);
             }
-            if (heightList[i] > heightList[i+1]) {
-                result.add(new int[]{i, heightList[i+1]});
+            int cur = pq.peek();//highest
+            if(prev != cur) {
+                result.add(new int[]{h[0], cur});
+                prev = cur;
             }
         }
-        result.add(new int[]{max, 0});
         return result;
     }
 }

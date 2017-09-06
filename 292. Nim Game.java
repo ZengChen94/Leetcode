986b7eb71e66fb88c5e0dfd9cd6280cad284828e
zcunique <zengchen94@gmail.com>
2017-09-06 08:56:23 -0500

    commit

--- 292. Nim Game.java
+++ 292. Nim Game.java
@@ -1,5 +1,6 @@
 public class Solution {
     public boolean canWinNim(int n) {
+		//always make the remaining to be factor of 4
         return n % 4 != 0;
     }
 }
--- 301. Remove Invalid Parentheses.java
+++ 301. Remove Invalid Parentheses.java
@@ -18,6 +18,7 @@ public class Solution {
             if (s.charAt(i) != '(' && s.charAt(i) != ')')
                 stringStart += s.charAt(i);
         }
+		
         for (int i = s.length()-1; i >= 0; i--) {
             if (s.charAt(i) == ')') {
                 end = i;

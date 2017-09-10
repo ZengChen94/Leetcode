0d31b3cdd62bfb07679cea91c90914d5f66ae959
zcunique <zengchen94@gmail.com>
2017-09-10 14:13:05 -0500

    dp

--- 312. Burst Balloons.java
+++ 312. Burst Balloons.java
@@ -1,17 +1,18 @@
 public class Solution {
     public int maxCoins(int[] nums) {
-        int[] arr = new int[nums.length+2];  
+        int[] arr = new int[nums.length+2];//add 1 on both sides
         for(int i = 1; i < nums.length + 1; ++i)
             arr[i] = nums[i-1];  
         arr[0] = arr[nums.length+1] = 1;  
           
-        int[][] dp = new int[nums.length+2][nums.length+2];  
+        int[][] dp = new int[nums.length+2][nums.length+2]; //represent the maximum value between left and right, but not include them!
         int n = nums.length+2;  
         
         for (int len = 2; len < n; len++) {
             for (int i = 0; i + len < n; i++) {
                 int j = i + len;
                 for (int k = i+1; k <= j-1; k++)
+					//dp[i][k] and dp[k][j] at first, and then multiple left arr[i], arr[k] and arr[j]
                     dp[i][j] = Math.max(dp[i][j], dp[i][k]+dp[k][j]+arr[i]*arr[k]*arr[j]);
             }
         }

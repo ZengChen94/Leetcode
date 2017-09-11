ffb2ecfab953b49411e5189ef9264eeb1f2a2fa8
zcunique <zengchen94@gmail.com>
2017-09-10 22:28:01 -0500

    dfs

---
+++ 320. Generalized Abbreviation.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> generateAbbreviations(string word) {
+        vector<string> res;
+        for (int i = 0; i < pow(2, word.size()); ++i) {
+            string out = "";
+            int cnt = 0, t = i;
+            for (int j = 0; j < word.size(); ++j) {
+                if (t & 1 == 1) {
+                    ++cnt;
+                    if (j == word.size() - 1) out += to_string(cnt);
+                } else {
+                    if (cnt != 0) {
+                        out += to_string(cnt);
+                        cnt = 0;
+                    }
+                    out += word[j];
+                }
+                t >>= 1;
+            }
+            res.push_back(out);
+        }
+        return res;
+    }
+};

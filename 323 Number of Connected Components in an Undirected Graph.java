724534325d589868f78acfaf70dee1f9ea467639
zcunique <zengchen94@gmail.com>
2017-09-10 22:57:23 -0500

    union set

---
+++ 323 Number of Connected Components in an Undirected Graph.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int[] id = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+        }
+        
+        // union
+        for (int[] edge : edges) {              
+            int i = root(id, edge[0]);
+            int j = root(id, edge[1]);
+            id[i] = j;
+        }
+        
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (id[i] == i)
+                count++;
+        }
+        return count;
+    }
+    
+    public int root(int[] id, int i) {
+        while (i != id[i]) {
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+}

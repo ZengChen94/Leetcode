15effe422f9c5c163aee3b3a716dafdbe6e3aff7
zcunique <zengchen94@gmail.com>
2017-09-11 21:01:03 -0500

    map to recored match targets

--- 336. Palindrome Pairs.java
+++ 336. Palindrome Pairs.java
@@ -4,17 +4,23 @@ public class Solution {
         if (words == null) 
             return pairs;
         HashMap<String, Integer> map = new HashMap<>();
+		//word, index
         for (int i = 0; i < words.length; ++ i) 
             map.put(words[i], i);
         for (int i = 0; i < words.length; ++ i) {
             int l = 0, r = 0;
             while (l <= r) {
                 String s = words[i].substring(l, r);
+				//search whether the reverse of the cut off part can be found in the map
                 Integer j = map.get(new StringBuilder(s).reverse().toString());
                 if (j != null && i != j && isPalindrome(words[i].substring(l == 0 ? r : 0, l == 0 ? words[i].length() : l)))
                     pairs.add(Arrays.asList(l == 0 ? new Integer[]{i, j} : new Integer[]{j, i}));
-                if (r < words[i].length()) ++r;
-                else ++l;
+                //take the front part
+				if (r < words[i].length()) 
+					r++;
+				//take the back part
+                else 
+					l++;
             }
         }
         return pairs;

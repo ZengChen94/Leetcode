bae5d3470149cf0f6c0b1e5e6235dc9104d9d8b7
zcunique <zengchen94@gmail.com>
2017-10-02 13:29:03 -0500

    expend right / restrict left

--- 340. Longest Substring with At Most K Distinct Characters.cpp
+++ 340. Longest Substring with At Most K Distinct Characters.cpp
@@ -4,8 +4,8 @@ public:
         int res = 0, left = 0;
         unordered_map<char, int> m;
         for (int i = 0; i < s.size(); ++i) {
-            ++m[s[i]];
-            while (m.size() > k) {
+            ++m[s[i]];//expend right
+            while (m.size() > k) {//restrict left
                 if (--m[s[left]] == 0) 
 					m.erase(s[left]);
                 ++left;

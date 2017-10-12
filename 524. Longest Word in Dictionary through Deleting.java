d2134a5d9d64d2465d303445f4d13935916708a0
zcunique <zengchen94@gmail.com>
2017-10-12 12:53:28 -0500

    can sort at first, but which is quicker cannot be decided

--- 524. Longest Word in Dictionary through Deleting.java
+++ 524. Longest Word in Dictionary through Deleting.java
@@ -1,6 +1,7 @@
 public class Solution {
     public String findLongestWord(String s, List<String> d) {
         String longest = "";
+		//here in fact we can sort the dict according from long to short
         for (String dictWord : d) {
             int i = 0;
             for (char c : s.toCharArray()) 

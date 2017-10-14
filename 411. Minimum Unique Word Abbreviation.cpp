306dceb5df26eacf586fd0ec721b729fa230b9e1
zcunique <zengchen94@gmail.com>
2017-10-14 09:40:03 -0500

    pruning + dfs

--- 411. Minimum Unique Word Abbreviation.cpp
+++ 411. Minimum Unique Word Abbreviation.cpp
@@ -1,59 +1,80 @@
-class Solution {
-public:
-    string minAbbreviation(string target, vector<string>& dictionary) {
-        if (dictionary.empty()) return to_string((int)target.size());
-        priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> q;
-        q = generate(target);
-        while (!q.empty()) {
-            auto t = q.top(); q.pop();
-            bool no_conflict = true;
-            for (string word : dictionary) {
-                if (valid(word, t.second)) {
-                    no_conflict = false;
-                    break;
-                }
-            }
-            if (no_conflict) return t.second;
-        }
-        return "";
-    }
-    priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> generate(string target) {
-        priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> res;
-        for (int i = 0; i < pow(2, target.size()); ++i) {
-            string out = "";
-            int cnt = 0, size = 0;
-            for (int j = 0; j < target.size(); ++j) {
-                if ((i >> j) & 1) ++cnt;
-                else {
-                    if (cnt != 0) {
-                        out += to_string(cnt);
-                        cnt = 0;
-                        ++size;
-                    }
-                    out += target[j];
-                    ++size;
-                }
-            }
-            if (cnt > 0) {
-                out += to_string(cnt);
-                ++size;
-            }
-            res.push({size, out});
-        }
-        return res;
-    }
-    bool valid(string word, string abbr) {
-        int m = word.size(), n = abbr.size(), p = 0, cnt = 0;
-        for (int i = 0; i < abbr.size(); ++i) {
-            if (abbr[i] >= '0' && abbr[i] <= '9') {
-                if (cnt == 0 && abbr[i] == '0') return false;
-                cnt = 10 * cnt + abbr[i] - '0';
-            } else {
-                p += cnt;
-                if (p >= m || word[p++] != abbr[i]) return false;
-                cnt = 0;
-            }
-        }
-        return p + cnt == m;
-    }
-};
+class Solution {  
+public:  
+    string minAbbreviation(string target, vector<string>& dictionary) {  
+        int len = target.size();  
+        if(len == 0) return "";  
+        vector<string> dic;  
+		// only the one has the same originally length, they may confict
+        for(string s:dictionary) {  
+            if(s.size() == len) {  
+                dic.push_back(s);  
+            }  
+        }  
+        int len_d = dic.size();  
+        if(len_d == 0) 
+			return intTostring(len);  
+        string res = target;  
+        dfs("", 0, target, 0, dic, res, len);  
+        return res;  
+    }  
+  
+    void dfs(string cur, int cur_len, string& target, int pos, vector<string>& dic, string& res, int& minlen) {  
+        if(pos >= (int)target.size()) {  
+            if(cur_len < minlen) {  //¼ôÖ¦  
+                bool f = true;  
+                for(string s : dic) {  
+                    if(check(s, cur)) {  
+                        f = false;
+						break;  
+                    }  
+                }  
+                if(f){  
+                    res = cur;  
+                    minlen = cur_len;  
+                }  
+            }  
+            return;  
+        }  
+        if(minlen == cur_len) return;   //¼ôÖ¦  
+        if(cur.empty() || !isdigit(cur.back())) {   //  
+            for(int i = target.size()-1; i >= pos; i--) {  
+                 string add = intTostring(i-pos+1);  
+                 dfs(cur+add, cur_len+1, target,i+1, dic, res, minlen);  
+            }  
+        }  
+        dfs(cur+target[pos], cur_len+1, target,pos+1, dic, res, minlen);  
+    }  
+  
+    bool check(string s1, string s2) {  
+        int len1 = s1.size();  
+        int len2 = s2.size();  
+        int l = 0,r = 0;  
+        while(l < len1 && r < len2) {  
+            if(isdigit(s2[r])) {  
+                int dis = 0;  
+                while(r < len2 && isdigit(s2[r])) {  
+                    dis = dis * 10 + s2[r++] - '0';  
+                }  
+                l+=dis;  
+            }  
+            else if(s2[r] == '0') 
+				return false;  
+            else {  
+                if(s1[l] == s2[r]) {  
+                    l++;
+					r++;  
+                }  
+                else return false;  
+            }  
+         }  
+         if(l >= len1 && r >= len2) 
+			 return true;  
+         return false;  
+    }  
+  
+    string intTostring(int num) {  
+        stringstream ss;  
+        ss<<num;  
+        return ss.str();  
+    }  
+};  

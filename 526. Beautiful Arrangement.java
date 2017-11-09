0cc7cfbf5a171e3e5b2efa58b051966fe86dc674
zcunique <zengchen94@gmail.com>
2017-11-09 15:23:24 -0600

    swap

--- 526. Beautiful Arrangement.java
+++ 526. Beautiful Arrangement.java
@@ -23,3 +23,38 @@ public class Solution {
         }
     }
 }
+
+public class Solution {
+    int cnt = 0;
+    
+    public int countArrangement(int N) {
+        if (N == 0) 
+            return 0;
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++)
+            nums[i] = i+1;
+        helper(nums, N-1);
+        return cnt;
+    }
+    
+    public void helper(int[] nums, int index) {
+        if (index == 0) {
+            cnt += 1;
+            return;
+        }
+        
+        for (int i = index; i >= 0; i--) {
+            if (nums[i] % (index+1) == 0 || (index+1) % nums[i] == 0) {
+                int tmp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = tmp;
+                helper(nums, index-1);
+                tmp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+        
+        return;
+    }
+}

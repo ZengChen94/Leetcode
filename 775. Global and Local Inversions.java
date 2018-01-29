57c7e0aa33cde47c37252370d72a56072e1c55ac
zcunique <zengchen94@gmail.com>
2018-01-29 14:34:48 -0600

    if (cmax > A[i + 2]) return false;

---
+++ 775. Global and Local Inversions.java
@@ -0,0 +1,11 @@
+// draw a line chart, and then the rule is obvious
+class Solution {
+    public boolean isIdealPermutation(int[] A) {
+        int cmax = 0;
+        for (int i = 0; i < A.length - 2; ++i) {
+            cmax = Math.max(cmax, A[i]);
+            if (cmax > A[i + 2]) return false;
+        }
+        return true;
+    }
+}
